c41dcfcf25d5e1677902b5ee60f40cea3aad1cfc
Emmanuelnnadi111 <doneonyi920@gmail.com>
2024-08-14 22:41:50 +0100

    fixing errors

--- src/app/page.js
+++ src/app/page.js
@@ -22,21 +22,48 @@
 //     </div>
 //   );
 // }
-import { use } from "react";
+// import { use } from "react";
+// import TodoList from "./components/todoList";
+
+// async function getTodos() {
+//   const res = await fetch("/api/todos", {
+//     cache: "no-store",
+//   });
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch todos");
+//   }
+//   return res.json();
+// }
+
+// export default function Home() {
+//   const todos = use(getTodos());
+
+//   return (
+//     <div>
+//       <h1 className="text-center py-20 text-3xl font-bold">Todo List</h1>
+//       <TodoList initialTodos={todos} />
+//     </div>
+//   );
+// }
 import TodoList from "./components/todoList";
 
 async function getTodos() {
-  const res = await fetch("/api/todos", {
+  const res = await fetch("http://localhost:3000/api/todos", {
     cache: "no-store",
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
+
   if (!res.ok) {
     throw new Error("Failed to fetch todos");
   }
+
   return res.json();
 }
 
-export default function Home() {
-  const todos = use(getTodos());
+export default async function Home() {
+  const todos = await getTodos();
 
   return (
     <div>
